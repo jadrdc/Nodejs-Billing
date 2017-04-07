1c9daeb7cc4a96c607b6f76442505a2ec4c1abdb
jadrdc <jadrdc@gmail.com>
2017-04-06 23:40:57 -0400

    Cambios Finales

--- config/parameter.js
+++ config/parameter.js
@@ -1,7 +1,7 @@
 const mongoose=require("mongoose");
 
 module.exports={
-  port : 9797,
+  port : process.env.PORT||8888,
   secret : "integracion",
   database: 'mongodb://root:root@ds019628.mlab.com:19628/bibliotecaunapec'
 ,mongoose
